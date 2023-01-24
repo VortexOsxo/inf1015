7aebb7dea60c4008c918c532ec38995b5ae373b2
Philippe Martin <philippeoliviermartin@gmail.com>
2023-01-24 17:23:21 -0500

    no

--- td1/exo3.cpp
+++ td1/exo3.cpp
@@ -2,8 +2,12 @@
 #include <string>
 #include <cmath>
 
+const float G = 9.80665;
+
 double vérifierValeur(std::string texte, double min, double max) {
-    double valeur = max + 1;
+    double valeur;
+    std::cout << texte << std::endl;
+    std::cin >> valeur;
     while (valeur < min or valeur > max) {
         std::cout << texte << std::endl;
         std::cin >> valeur;
@@ -13,11 +17,22 @@ double vérifierValeur(std::string texte, double min, double max) {
 
 
 int main() {
-    int hauteurInitiale, nRebond;
-    float coefficientRebond;
+    int nRebond;
+    float hauteurInitiale, coefficientRebond;
 
     hauteurInitiale = vérifierValeur("Entrer la hauteur initiale: ", 0, INFINITY);
-    return 0;
+    nRebond = vérifierValeur("Entrer le nombre de rebond après lequelle vouz-voulez la hauteur: ", 0, INFINITY);
+    coefficientRebond = vérifierValeur("Entrer le coefficient de rebond",0,1);
+    float vitesseInitiale, vitesseFinale, hauteurFinale;
+    for (int i = 0; i < nRebond; i--) {
+        vitesseInitiale = std::pow((2*G*hauteurInitiale), 0.5);
+        vitesseFinale = coefficientRebond * vitesseInitiale;
+        hauteurFinale = (float) (std::pow(vitesseFinale,2)) / (2 * G);
 
+        hauteurInitiale = hauteurFinale;
+    }
+
+    std::cout << "La hauteur finale est :" << hauteurFinale << std::endl;
+    return 0;
 }
 
