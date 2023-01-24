4d4d9fd4d68456d2d77009c860a3f6106d6d79bb
SkanderH265 <skander.hel@gmail.com>
2023-01-24 17:18:00 -0500

    yo

--- td1/exo2.cpp
+++ td1/exo2.cpp
@@ -9,40 +9,34 @@ using std::endl;
 //mois n-1
 // moisn = moins n-1 - paiement + moisn-1 * taux/12
 
+double verifierValeur(std::string texte, double min, double max) {
+    double valeur = max + 1;
+    while ((valeur < min) || (valeur > max)) {
+        std::cout << texte << std::endl;
+        std::cin >> valeur;
+    }
+    return valeur;
+}
+
 void emprunt(){  
 
-    double sommePret, paiementMois, tauxAnnuel;
+    double sommePret = verifierValeur("La somme d'argent prétée : ", 0, INFINITY);
+    double paiementMois = verifierValeur("Le montant remboursé chaque mois : ", 0, INFINITY);
+    double tauxAnnuel = verifierValeur("Taux d'interets annuels = ", 0, INFINITY);
 
-    cout << "La somme d'argent prétée : "<< endl;
-    cin >> sommePret;
-
-    cout << "Le montant remboursé chaque mois : "<< endl;
-    cin >> paiementMois;
-
-    cout << "Taux d'interets annuels : "<< endl;
-    cin >> tauxAnnuel;
-   
-    int nombreMois = 0;
-    double sommeInterets, sommeRestante = sommePret, interetsMois, tauxMensuel;
-
+    int nombreMois; double sommeInterets, sommeRestante = sommePret, interetsMois, tauxMensuel;
 
     while (sommePret >= 0) { 
 
-       
         tauxMensuel = sommePret * tauxAnnuel / 100 / 12;
         sommePret = sommePret - paiementMois + tauxMensuel;
         sommeInterets += tauxMensuel;
         nombreMois +=1;  
-        
-        
 
     }
         
-
     cout << "Nombre de mois nécessaires pour rembourser la dette : " << nombreMois << endl;
     cout << "Somme d'interets perçus par le preteur: " << sommeInterets << endl;
-
-
 }
 
 int main(){
