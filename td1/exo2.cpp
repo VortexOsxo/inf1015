54c6b272fb0f2aad00ebac2028f6d8d82ba37c75
Philippe Martin <philippeoliviermartin@gmail.com>
2023-01-24 18:03:23 -0500

    Yo

--- td1/exo2.cpp
+++ td1/exo2.cpp
@@ -6,9 +6,6 @@ using std::string;
 using std::cout; using std::cin;
 using std::endl;
 
-//mois n-1
-// moisn = moins n-1 - paiement + moisn-1 * taux/12
-
 double verifierValeur(std::string texte, double min, double max) {
     double valeur;
     std::cout << texte << std::endl;
@@ -20,33 +17,21 @@ double verifierValeur(std::string texte, double min, double max) {
     return valeur;
 }
 
-
-void emprunt(){  
-
-
+int main(){
     double sommePret = verifierValeur("La somme d'argent prétée : ", 0, INFINITY);
     double paiementMois = verifierValeur("Valeur du paiement par mois : ", 0, INFINITY);
     double tauxAnnuel = verifierValeur("Taux interets annuels : ", 0, INFINITY);
    
     int nombreMois = 0;
-    double sommeInterets, sommeRestante, interetsMois, tauxMensuel;
-
+    double sommeInterets, interetsMois, tauxMensuel;
 
     while (sommePret >= 0){ 
         tauxMensuel = sommePret * tauxAnnuel / 100 / 12;
         sommePret = sommePret - paiementMois + tauxMensuel;
         sommeInterets += tauxMensuel;
         nombreMois +=1;  
-
     }
         
-
     cout << "Nombre de mois nécessaires pour rembourser la dette : " << nombreMois << endl;
     cout << "Somme d'interets perçus par le preteur: " << sommeInterets << endl;
-
-
-}
-
-int main(){
-    emprunt();
 }
