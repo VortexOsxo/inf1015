8b44b123fe27a5b1ecb3c1c147f58135cb9cc4d1
Philippe Martin <philippeoliviermartin@gmail.com>
2023-01-24 16:57:03 -0500

    Yo

---
+++ td1/exo3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+double vérifierValeur(std::string texte, double min, double max) {
+    double valeur = max + 1;
+    while (valeur < min or valeur > max) {
+        std::cout << texte << std::endl;
+        std::cin >> valeur;
+    }
+    return valeur;
+}
+
+
+int main() {
+    int hauteurInitiale, nRebond;
+    float coefficientRebond;
+
+    hauteurInitiale = vérifierValeur("Entrer la hauteur initiale: ", 0, INFINITY);
+    return 0;
+
+}
+
