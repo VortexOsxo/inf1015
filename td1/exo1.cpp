306321793473d614e9a42a8cb5c83dfceb79b117
Philippe Martin <philippeoliviermartin@gmail.com>
2023-01-25 13:06:33 -0500

    yes

--- td1/exo1.cpp
+++ td1/exo1.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <math.h>
 
-int premier () {
+int main () {
     std::cout << "Entrer un nombre entier : " << std::endl;
     int nombre;
     std::cin >> nombre;
     int racineArrondie = std::pow(nombre, 0.5);
     if (nombre % 2 == 0) {
-        std::cout << "Ce nombre n'est pas premier car il est divisible par 2." << std::endl;
+        std::cout << "Ce nombre n'est pas premier, car il est divisible par 2." << std::endl;
         return 0;
     }
     for (int i = 3; i <= racineArrondie; i += 2) {
         if (nombre % i == 0) {
-            std::cout << "Ce nombre n'est pas premier car il est divisible par " << i << "." << std::endl;
+            std::cout << "Ce nombre n'est pas premier, car il est divisible par " << i << "." << std::endl;
             return 0;
         }
     }
